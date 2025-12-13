57cd90cec7add08c3347e1ad075959fd1e9c60ca
Ahmed-Ossamaa <aossama2015@gmail.com>
2025-12-13 08:06:30 +0200

    imported payment routes and webhook in app.js

--- Backend/src/Validation/lessonValidation.js
+++ Backend/src/Validation/lessonValidation.js
@@ -5,17 +5,14 @@ const createLessonSchema = Joi.object({
     title: Joi.string().required().messages({
         'string.empty': 'Title is required'
     }),
-    videoUrl: Joi.string().uri().required().messages({
-        'string.empty': 'Video URL is required',
-        'string.uri': 'Video URL must be valid'
-    }),
-    duration: Joi.number().min(1).required().messages({
+
+    duration: Joi.number().min(1).messages({
         'number.base': 'Duration must be a number',
         'number.min': 'Duration must be positive'
     }),
     resources: Joi.array().items(Joi.object({ fileUrl: Joi.string().uri(), filePublicId: Joi.string() })),
     content: Joi.string().allow(''),
-    course: objectIdValidator.required(),
+    // course: objectIdValidator.required(),
     section: objectIdValidator.required().messages({
         'string.empty':'CourseId is required'
     })
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,19 +11,24 @@ const categoryRoutes = require('./routes/categoryRoutes');
 const lessonRoutes = require('./routes/lessonRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
 const sectionRoutes = require('./routes/sectionRoutes');
+const enrollmentRoutes = require('./routes/enrollmentRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
+const stripeWebhookRoute = require('./routes/stripeWebhook');
 const ApiError = require('./utils/ApiError');
 const errorHandler = require('./middlewares/errorHandler');
 const cookieParser = require("cookie-parser");
 
 const app = express();
 db.connect("Mongo");
+//=============================== Stripe Webhook ================================
+app.use("/api/v1/webhook", stripeWebhookRoute);
 
 //=============================== Middlewares ===================================
 app.use(cors(
     {
         origin: process.env.FRONTEND_URL || 'http://localhost:3000',
         credentials: true
-    }
+    }   
 ));
 app.use(express.json());
 app.use(cookieParser());
@@ -32,11 +37,13 @@ app.use(cookieParser());
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/courses', courseRoutes);
+app.use('api/v1/enrollments', enrollmentRoutes);
 app.use('/api/v1/categories', categoryRoutes);
 app.use('/api/v1/lessons', lessonRoutes);
 app.use('/api/v1/reviews', reviewRoutes);
 app.use('/api/v1/sections', sectionRoutes);
 app.use('/api/v1/feedback', feedbackRoutes);
+app.use("/api/v1/payments", paymentRoutes);
 
 app.all(/.*/, (req, res, next) => {
     throw  ApiError.notFound(`Path ${req.originalUrl} not found`);
