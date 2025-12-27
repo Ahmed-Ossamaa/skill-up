548d8fc3195e709f840d8e69e2a54ce48ddd99d2
Ahmed-Ossamaa <aossama2015@gmail.com>
2025-12-27 14:31:50 +0200

    Fix: Live email service

--- Backend/src/utils/sendEmail.js
+++ Backend/src/utils/sendEmail.js
@@ -4,11 +4,15 @@ const ApiError = require('./ApiError');
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: false,
+  secure: Number(process.env.EMAIL_PORT) === 465,
   auth: {
     user: process.env.EMAIL_USERNAME,
     pass: process.env.EMAIL_PASSWORD,
   },
+  tls: {
+    ciphers: 'SSLv3',
+    rejectUnauthorized: false
+  }
 });
 
 async function sendEmail({ to, subject, html, text }) {
